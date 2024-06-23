abd1058710bd3260642de97ba534ba7a5fb770bf
soma <swfh2367@gmail.com>
2024-06-24 02:11:39 +0900

    fix index.ts

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,15 +4,15 @@ import postgres from "postgres";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn =
+  globalForDb.conn ??
+  postgres(env.DATABASE_URL, {
+    ssl: env.NODE_ENV === "production",
+  });
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
